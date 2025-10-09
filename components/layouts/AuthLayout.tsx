dcc1d4d9c758caa9bb9bd65c681fa3171afc104e
Nizam_V <nizamvali1104@gmail.com>
2025-10-09 15:54:54 +0530

    fix build errors

--- components/layouts/AuthLayout.tsx
+++ components/layouts/AuthLayout.tsx
@@ -25,7 +25,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
           >
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg>
-          auth js, prisma, supabase template
+          Momentum
         </div>
       </div>
       <div className="flex items-center justify-center py-12">
